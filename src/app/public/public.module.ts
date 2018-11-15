12ff98d4b3b02a0aeb85f2175b8d20b941fa1ed7
BERT\Eddie <me@eddieabbondanz.io>
2018-11-14 21:32:55 -0500

    Added background image to login page

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,12 +4,14 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PublicRoutingModule } from './public-routing.module';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PublicRoutingModule
   ],
-  declarations: [NotFoundComponent, HomeComponent, NavBarComponent]
+  declarations: [NotFoundComponent, HomeComponent, NavBarComponent, LoginComponent, SignupComponent]
 })
 export class PublicModule { }
